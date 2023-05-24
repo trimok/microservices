5e0886b361b7da5e5ec07160ccc4ac0475a695a2
trimok <trimok@mail.fr>
2023-05-24 17:15:39 +0200

    Ajout serveur eureka

---
+++ microservice-eurekaserver/src/main/java/com/sante/eurekaserver/MicroserviceEurekaserverApplication.java
@@ -0,0 +1,15 @@
+package com.sante.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceEurekaserverApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(MicroserviceEurekaserverApplication.class, args);
+    }
+
+}
