8378e3eab01ddb41b237998989c511d120344592
trimok <trimok@mail.fr>
2023-05-25 12:19:57 +0200

    Correction Configuration docker erika-server

---
+++ docker-microservices.bat
@@ -0,0 +1,14 @@
+cd microservice-configserver
+call mvn package
+cd ../microservice-eurekaserver
+call mvn package
+cd ../microservice-patient
+call mvn package
+cd ../microservice-patienthistory
+call mvn package
+cd ../microservice-expert
+call mvn package
+cd ../microservice-clientui
+call mvn package
+cd ..
+docker compose up
