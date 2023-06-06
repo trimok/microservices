1de8f9677a97d461c1bb5404cdfd7a724280135a
trimok <trimok@mail.fr>
2023-06-06 16:29:40 +0200

    Delete batch

--- docker-compose-keycloak.bat
+++
@@ -1,1 +0,0 @@
-docker-compose -p keycloak -f docker-compose-keycloak.yml up
--- docker-keycloak-run.bat
+++
@@ -1,1 +0,0 @@
-docker run -d --name keycloak -p 8888:8080  -e KEYCLOAK_USER=spring -e KEYCLOAK_PASSWORD=spring123 jboss/keycloak
