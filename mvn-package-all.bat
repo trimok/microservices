b2be4b5025531cba99247936795914772e143f22
trimok <trimok@mail.fr>
2023-06-20 11:41:23 +0200

    Première version du script global package

--- mvn-package-all.bat
+++ mvn-package-all.bat
@@ -5,61 +5,73 @@ set NLM=^
 
 set NL=^^^%NLM%%NLM%^%NLM%%NLM%
 
+echo %NL%%NL%%NL%    ************************************%NL%    mvn package configserver%NL%    ************************************%NL%%NL%
 cd microservice-configserver
 call mvn package
 if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn package configserver%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
+echo %NL%%NL%%NL%    ************************************%NL%    mvn package eurekaserver%NL%    ************************************%NL%%NL%
 cd ../microservice-eurekaserver
 call mvn package
 if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn package eurekaserver%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
+echo %NL%%NL%%NL%    ************************************%NL%    mvn package oauth2server%NL%    ************************************%NL%%NL%
 cd ../microservice-oauth2server
 call mvn package 
 if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn package oauth2server%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
+echo %NL%%NL%%NL%    ************************************%NL%    mvn package gateway%NL%    ************************************%NL%%NL%
 cd ../microservice-gateway
 call mvn package 
 if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn package gateway%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
+echo %NL%%NL%%NL%    ************************************%NL%    mvn package patient%NL%    ************************************%NL%%NL%
 cd ../microservice-patient
 call mvn package 
 if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn package patient%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
+echo %NL%%NL%%NL%    ************************************%NL%    mvn package patienthistory%NL%    ************************************%NL%%NL%
 cd ../microservice-patienthistory
 call mvn package 
 if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn package patienthistory%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF 
+echo %NL%%NL%%NL%    ************************************%NL%    mvn package expert%NL%    ************************************%NL%%NL%
 cd ../microservice-expert
 call mvn package
 if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn package expert%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
-cd ../microservice-configserver
+echo %NL%%NL%%NL%    ************************************%NL%    spring boot run configserver %NL%    ************************************%NL%%NL%
+cd ../microservice-configserver -Dspring-boot.run.profiles=test
 start /b mvn spring-boot:run
 ping 127.0.0.1 -n 15 > nul
-if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn spring-boot:run configserver%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF 
-cd ../microservice-eurekaserver
+echo %NL%%NL%%NL%    ************************************%NL%    spring boot run eurekaserver  %NL%    ************************************%NL%%NL%
+cd ../microservice-eurekaserver -Dspring-boot.run.profiles=test
 start /b mvn spring-boot:run
 ping 127.0.0.1 -n 15 > nul
-if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn spring-boot:run eurekaserver%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
-cd ../microservice-oauth2server
+echo %NL%%NL%%NL%    ************************************%NL%    spring boot run oauth2server%NL%    ************************************%NL%%NL%
+cd ../microservice-oauth2server -Dspring-boot.run.profiles=test
 start /b mvn spring-boot:run
 ping 127.0.0.1 -n 15 > nul
-if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn spring-boot:run oauth2server%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
+echo %NL%%NL%%NL%    ************************************%NL%    spring boot run gateway %NL%    ************************************%NL%%NL%
 cd ../microservice-gateway
-start /b mvn spring-boot:run
+start /b mvn spring-boot:run -Dspring-boot.run.profiles=test
 ping 127.0.0.1 -n 15 > nul
-if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn spring-boot:run gateway%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
+echo %NL%%NL%%NL%    ************************************%NL%    spring boot run patient %NL%    ************************************%NL%%NL%
 cd ../microservice-patient
 start /b mvn spring-boot:run -Dspring-boot.run.profiles=test
 ping 127.0.0.1 -n 15 > nul
-if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn spring-boot:run patient%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
+echo %NL%%NL%%NL%    ************************************%NL%    spring boot run patienthistory %NL%    ************************************%NL%%NL%
 cd ../microservice-patienthistory
 start /b mvn spring-boot:run -Dspring-boot.run.profiles=test
 ping 127.0.0.1 -n 15 > nul
-if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn spring-boot:run patienthistory%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
+echo %NL%%NL%%NL%    ************************************%NL%    spring boot run expert %NL%    ************************************%NL%%NL%
 cd ../microservice-expert
 start /b mvn spring-boot:run -Dspring-boot.run.profiles=test
 ping 127.0.0.1 -n 15 > nul
-if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn spring-boot:run expert%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
+echo %NL%%NL%%NL%    ************************************%NL%    spring boot run clientui %NL%    ************************************%NL%%NL%
 cd ../microservice-clientui
-start /b mvn spring-boot:run
+start /b mvn spring-boot:run -Dspring-boot.run.profiles=test
 ping 127.0.0.1 -n 15 > nul
-if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn spring-boot:run clientui%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
-rem call mvn package
-rem if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn package clientui%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
+echo %NL%%NL%%NL%    ************************************%NL%    mvn package clientui  %NL%    ************************************%NL%%NL%
+call mvn package
+if errorlevel 1 echo  %NL%%NL%%NL%    ************************************%NL%    Erreur dans  mvn package clientui%NL%    ************************************%NL%%NL% & cd.. & pause & goto :EOF
 cd..
+echo %NL%%NL%%NL%    ************************************%NL%    SUCCESS SUCCESS  %NL%    ************************************%NL%%NL%
+pause
+exit 0 /b
 :EOF
 pause
+exit 1 /b
